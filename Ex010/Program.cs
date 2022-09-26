dcc4004e06850271a1f7f287366ea3f1177c33a4
Julia Debelova <u.deb@mail.ru>
2022-09-26 15:31:37 +0500

    add ex010 about methods and for{}, while{}

---
+++ Ex010/Program.cs
@@ -0,0 +1,25 @@
+﻿void Method1(string msg, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+Method1(msg: "Привет " + Method3(), count: 2);
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
