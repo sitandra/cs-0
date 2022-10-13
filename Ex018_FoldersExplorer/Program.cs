2de1d9bb0eb75e3f7e46781048ca35e2a31248da
Julia Debelova <u.deb@mail.ru>
2022-10-13 19:48:03 +0500

    edit example 018 add task about towers and trees

--- Ex018_FoldersExplorer/Program.cs
+++ Ex018_FoldersExplorer/Program.cs
@@ -13,3 +13,30 @@
 }
 string path = @"/Work/Projects/cs-0/Ex018_FoldersExplorer";
 CatalogInfo(path);
+
+// игра в пираимидки
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
+
+// обход дерева
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// 0 1 2 3 4 5 6 7 8 9 10 11
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
