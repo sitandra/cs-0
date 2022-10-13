e053dc4e83c796c879e2140579cf21545bd67fbc
Julia Debelova <u.deb@mail.ru>
2022-10-13 18:24:50 +0500

    add example 016 about string parsing

---
+++ Ex016_StringParsing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+              .Replace("(","")
+              .Replace(")","");
+
+var data = text.Split(" ")  // Распарсить строку в пары координат
+               .Select(item => item.Split(','))  // Распарсить координаты в структуру
+               .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1]))) // Превратить координаты в коллекцию x, y
+               .Where(e => e.x % 2 == 0) // Отсеевает выборку по условию
+               .Select(point => (point.x*10, point.y)) // Умножить первую координату на 10
+               .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
