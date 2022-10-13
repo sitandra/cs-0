b35822c28caa7510bdff9416b46447c673b78d10
Julia Debelova <u.deb@mail.ru>
2022-10-13 18:48:15 +0500

    add example 018 about folder and file explorer

---
+++ Ex018_FoldersExplorer/Program.cs
@@ -0,0 +1,15 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"/Work/Projects/cs-0/Ex018_FoldersExplorer";
+CatalogInfo(path);
